d1ca9b996da6168550a1381cc79f76cecb01b7fc
dmitriiaristov <mitya.aristov@yandex.ru>
2025-02-25 18:46:17 +0100

    modified MapSchema.java: change types; modified ValidatorMapSchemaTest.java: remove testDataSchemaStringNumber method;

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -5,26 +5,26 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
-    public void addPredicate(String key, Predicate<Map<K, V>> predicate) {
+public class MapSchema<T, S> extends BaseSchema<Map<T, T>> {
+    public void addPredicate(String key, Predicate<Map<T, T>> predicate) {
         check.put(key, predicate);
     }
 
-    public MapSchema<K, V> required() {
-        Predicate<Map<K, V>> required = Objects::nonNull;
+    public MapSchema<T, S> required() {
+        Predicate<Map<T, T>> required = Objects::nonNull;
         addPredicate("required", required);
         return this;
     }
 
-    public MapSchema<K, V> sizeof(Integer numberOfElements) {
-        Predicate<Map<K, V>> sizeof = mapToValidate ->
+    public MapSchema<T, S> sizeof(Integer numberOfElements) {
+        Predicate<Map<T, T>> sizeof = mapToValidate ->
                 mapToValidate != null && mapToValidate.size() == numberOfElements;
         addPredicate("sizeof", sizeof);
         return this;
     }
 
-    public MapSchema<K, V> shape(Map<K, BaseSchema<V>> schema) {
-        Predicate<Map<K, V>> shape = detailsMapToValidate -> {
+    public MapSchema<T, S> shape(Map<T, BaseSchema<T>> schema) {
+        Predicate<Map<T, T>> shape = detailsMapToValidate -> {
             var values = new ArrayList<Boolean>();
             detailsMapToValidate.forEach((key, value) -> {
                 if (schema.containsKey(key)) {
